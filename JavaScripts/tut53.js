4850d4ca04a6febd5d8fb84c8c66a4b8df701268
kamleshkumarkohli <kamleshkumarkohli.cse2019@dscet.ac.in>
2023-02-14 14:51:27 +0530

    javaScripts functions Arrays and variables

---
+++ JavaScripts/tut53.js
@@ -0,0 +1,65 @@
+// console.log("this is tut53 ");
+
+  function greet(name ,messege){
+
+    let myvar = "unblock function";
+
+    console.log(name+ " " +messege);
+    console.log(name+ " is good boy");
+
+
+  }
+
+  function sum(a,b){
+    let c = a+b;
+    return c;
+  }
+  
+  function maxNum(a,b){
+   if(a>b){
+    return a;
+   }
+   else{
+    return b;
+   }
+
+  }
+  function squarNum(num1 , num2){
+    let squarNum = num1**num2;
+    return squarNum;
+  }
+
+  function rectArea(a,b){
+    return `the area of the reactangle is ${a*b}`
+  }
+
+  //to find the area of reactangle
+  console.log(rectArea(2,3));
+
+//    let name1 ="kamlesh";
+//    let name2 ="Ranjan";
+//    let name3 ="Raman";
+//    let name4 ="kundan";
+
+//    let messege= "you all win dude!";
+
+//    greet(name1 ,messege );
+//    greet(name2 ,messege );
+//    greet(name3 ,messege );
+//    greet(name4 ,messege );
+
+//   let add= sum(2,4);
+//   console.log("sum of two numbers is " +add);
+
+//   //to print max between two numbers
+//   let numbers = maxNum(56,9);
+
+//   console.log(numbers);
+
+//   // to find square of the two numbers 
+
+//   let sqrNumbers = squarNum(2,2);
+//   console.log(sqrNumbers);
+
+
+
---
+++ JavaScripts/tut54.js
@@ -0,0 +1,43 @@
+console.log("this is tut 54");
+
+//Aleart function in browser using javascripts
+
+// NOTE>> it does not return anythings
+
+   alert("this is alert function")
+
+
+//prompt function used in games to store name of the user
+
+ let Name = prompt("what is your name ", "Guest");
+ console.log(Name);
+
+confirm in js
+
+     let deletePost = confirm("Do you really want to delete this post? ");
+//       console.log(deletePost);
+
+//to delete the post 
+
+     if(deletePost){
+         console.log("your post has been deleted successfully!");
+     }
+
+ //to not delete the post
+
+     else{
+         console.log("your post has not been deleted !");   
+
+     }
+
+let entryCondition = prompt("Your Age?" , "18+");
+if(entryCondition){
+    console.log("your welcome into the club")
+
+}
+else{
+    console.log("sorry! your age is too short")
+}
+
+
+
